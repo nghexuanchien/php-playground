722a8997c8bf40c1e0d620ce199ac8a56a5f2ac2
chien <chienptit@gmail.com>
2019-12-19 16:28:36 +0700

    add blog module

--- asgardcms_sample/Themes/Flatly/views/layouts/master.blade.php
+++ asgardcms_sample/Themes/Flatly/views/layouts/master.blade.php
@@ -7,9 +7,9 @@
     @show
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@section('title')@setting('core::site-name')@show</title>
-    @foreach($alternate as $alternateLocale=>$alternateSlug)
-        <link rel="alternate" hreflang="{{$alternateLocale}}" href="{{url($alternateLocale.'/'.$alternateSlug)}}">
-    @endforeach
+{{--    @foreach($alternate as $alternateLocale=>$alternateSlug)--}}
+{{--        <link rel="alternate" hreflang="{{$alternateLocale}}" href="{{url($alternateLocale.'/'.$alternateSlug)}}">--}}
+{{--    @endforeach--}}
     <link rel="canonical" href="{{url()->current()}}" />
     <link rel="shortcut icon" href="{{ Theme::url('favicon.ico') }}">
 
